dbfb6642b8e1765854bed6b0bb721d542ee2e706
joshuakalungi <joshuakalungi101@gmail.com>
2024-02-05 16:04:43 +0300

    everything works perfectly

---
+++ toDo/src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+
+// eslint-disable-next-line react/prop-types
+function Header({ children }){
+    return(
+        <div className="Header">
+            {
+                children
+            }
+        </div>
+    )
+}
+
+export default Header;
---
+++ toDo/src/components/Main.jsx
@@ -0,0 +1,14 @@
+
+
+// eslint-disable-next-line react/prop-types
+function Main({ children }){
+    return(
+        <div className="Header">
+            {
+                children
+            }
+        </div>
+    )
+}
+
+export default Main;
