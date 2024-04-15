f4269f2a3f8b0efccb7065add33df4b57d93aae3
joshuakalungi <joshuakalungi101@gmail.com>
2024-04-15 22:51:03 +0300

    addnewtodo

--- toDo/src/components/AddNewTodo.jsx
+++ toDo/src/components/AddNewTodo.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Modal from './Modal';
-import { Bell } from 'react-bootstrap-icons';
+import { Bell, CalendarDay, Clock, Palette, X } from 'react-bootstrap-icons';
 
 
 
@@ -24,8 +24,42 @@ function AddNewTodo(){
                     </div>
                     <div className='remind'>
                         <Bell />
-                        <p> Remind Me!</p>git
+                        <p> Remind Me!</p>
                     </div>
+                    <div className="pick-day">
+                        <div className="title">
+                            <CalendarDay />
+                            <p> Choose a day</p>
+                        </div>
+                    </div>
+                    Date picker
+                    <div className="pick-time">
+                        <div className="title">
+                            <Clock />
+                            <p> Choose the time</p>
+                        </div>
+                    </div>
+                    Time picker
+                    <div className="pick-project">
+                        <div className="title">
+                            <Palette />
+                            <p> Choose a project</p>
+                        </div>
+                        <div className="projects">
+                        <div className="project active">
+                            personal
+                        </div>
+                        <div className="project">
+                            work
+                        </div>
+                    </div>
+                    </div>
+                   <div className="cancel" onClick={()=> setShowModal(false)}>
+                    <X size='40'/>
+                    </div>
+                <div className="confirm">
+                    <button> + Add to do </button>
+                </div>
                 </form>
             </Modal>
         </div>
