3d72d8ca5c8adb8cd6b50333773bd476f8fea342
joshuakalungi <joshuakalungi101@gmail.com>
2024-04-17 18:17:01 +0300

    datepicker added

--- datePicker/src/App.jsx
+++ datePicker/src/App.jsx
@@ -1,9 +1,14 @@
+import React, { useState } from "react";
+import Datepicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 function App() {
+  const [selectedDate, setSelectedDate] = useState(null)
 
   return (
     <>
       <div className="App">
         <p>Hello</p>
+        <Datepicker selected={selectedDate} onChange={(date)=>setSelectedDate(date)}/>
       </div>
     </>
   )
