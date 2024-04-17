991e587c00fd45514d101b7d6ecedc66a2c5b060
joshuakalungi <joshuakalungi101@gmail.com>
2024-04-17 18:49:08 +0300

    finaled

--- datePicker/src/App.jsx
+++ datePicker/src/App.jsx
@@ -1,14 +1,32 @@
 import React, { useState } from "react";
 import Datepicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {FaCalendarAlt} from "react-icons/fa";
+import "./App.css";
+
+function CustomInput({value, onClick}){
+  return(
+
+    <div className="input-group">
+      <input type="text" className="form-control" value={value} onClick={onClick} readOnly></input>
+      <div className="input-group-append">
+        <span className="input-group-text">
+          <FaCalendarAlt/>
+        </span>
+      </div>
+    </div>
+  )
+}
+
 function App() {
   const [selectedDate, setSelectedDate] = useState(null)
 
   return (
     <>
       <div className="App">
-        <p>Hello</p>
-        <Datepicker selected={selectedDate} onChange={(date)=>setSelectedDate(date)}/>
+        <h1 className="mt-2 mb-5">React Datepicker with Calendar Icon</h1>
+        <label><Datepicker selected={selectedDate} onChange={(date)=>setSelectedDate(date)} customInput={<CustomInput/>}/></label>
       </div>
     </>
   )
