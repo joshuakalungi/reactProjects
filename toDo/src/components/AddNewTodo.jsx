97937b4c4bd7c221960a7008899297dd0a264a6b
joshuakalungi <joshuakalungi101@gmail.com>
2024-04-18 23:44:03 +0300

    added the date and timepickers

--- toDo/src/components/AddNewTodo.jsx
+++ toDo/src/components/AddNewTodo.jsx
@@ -1,13 +1,33 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Modal from './Modal';
 import { Bell, CalendarDay, Clock, Palette, X } from 'react-bootstrap-icons';
+import Datepicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {FaCalendarAlt} from "react-icons/fa";
+import { TimePickerComponent } from '@syncfusion/ej2-react-calendars';
 
+function CustomInput({value, onClick}){
+  return(
+
+    <div className="input-group">
+      <input type="text" className="form-control" value={value} onClick={onClick} readOnly></input>
+      <div className="input-group-append">
+        <span className="input-group-text">
+          <FaCalendarAlt/>
+        </span>
+      </div>
+    </div>
+  )
+}
 
 
 // eslint-disable-next-line react/prop-types
 function AddNewTodo(){
     const [showModal, setShowModal] = useState(false);
     const [text, setText] = useState('');
+    const [selectedDate, setSelectedDate] = useState(null)
+
 
     return(
         <div className="AddNewTodo">
@@ -17,7 +37,7 @@ function AddNewTodo(){
                 </button>
             </div>
             <Modal showModal={showModal} setShowModal={setShowModal}>
-                <form>
+                <form className="container">
                     <div className='text'>
                         <h3>Add new to do</h3>
                         <input type='text' value={text} onChange={(e)=>setText(e.target.value)} placeholder='To do ...' autoFocus/>
@@ -32,14 +52,16 @@ function AddNewTodo(){
                             <p> Choose a day</p>
                         </div>
                     </div>
-                    Date picker
+                    <div className="date">
+                        <label><Datepicker selected={selectedDate} onChange={(date)=>setSelectedDate(date)} customInput={<CustomInput/>}/></label>
+                    </div>
                     <div className="pick-time">
                         <div className="title">
                             <Clock />
                             <p> Choose the time</p>
                         </div>
                     </div>
-                    Time picker
+                    <TimePickerComponent placeholder='Select a time'></TimePickerComponent>
                     <div className="pick-project">
                         <div className="title">
                             <Palette />
