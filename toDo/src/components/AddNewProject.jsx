3cae5ad556b0521721259ec0c1570ba57264e553
joshuakalungi <joshuakalungi101@gmail.com>
2024-05-09 16:45:59 +0300

    logic added

--- toDo/src/components/AddNewProject.jsx
+++ toDo/src/components/AddNewProject.jsx
@@ -1,15 +1,33 @@
 import { Plus } from "react-bootstrap-icons";
-
+import ProjectForm from "./ProjectForm";
+import Modal from './Modal';
+import { useState } from "react";
 
 // eslint-disable-next-line react/prop-types
 function AddNewProject(){
+    const [projectName, setProjectName] = useState('')
+    const [showModal, setShowModal]= useState(false)
+
+    function handleSubmit(e) {
+
+    }
     return(
         <div className="AddNewProject">
             <div className="add-button">
-                <span>
+                <span onClick={()=>setShowModal(true)}>
                     <Plus size="20"/>
                 </span>
             </div>
+            <Modal showModal={showModal} setShowModal={setShowModal}>
+                <ProjectForm 
+                handleSubmit={handleSubmit}
+                heading='New project!'
+                value={projectName}
+                setValue={setProjectName}
+                setShowModal={setShowModal}
+                confirmButtonText="+ Add Project"
+                />
+            </Modal>
         </div>
     )
 }
---
+++ toDo/src/components/ProjectForm.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+// eslint-disable-next-line react/prop-types
+const ProjectForm = ({handleSubmit, heading, value, setValue, setShowModal, confirmButtonText}) => {
+  return (
+    <form onSubmit={handleSubmit}>
+        <h3>{heading}</h3>
+        <input 
+            value={value}
+            onClick={(e)=> setValue(e.target.value)}
+            type='text'
+            placeholder='project name....'
+            autoFocus
+        />
+        <button className="cancel" role="button" onClick={() => setShowModal(false)}>
+            Cancel
+        </button>
+        <button className='confirm'>
+            {confirmButtonText}
+        </button>
+    </form>
+  )
+}
+
+export default ProjectForm
